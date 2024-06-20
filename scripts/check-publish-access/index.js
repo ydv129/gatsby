e5ef2f51ede42f77e2b6310f438556496828f7b3
ydv129 <169161801+ydv129@users.noreply.github.com>
2024-06-20 17:23:51 +0530

    Update index.js
    More precise code

--- scripts/check-publish-access/index.js
+++ scripts/check-publish-access/index.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
-const getUnownedPackages = require(`../get-unowned-packages`)
+const getUnownedPackages = require('../get-unowned-packages');
 
-getUnownedPackages()
-  .then(({ packages, user }) => {
-    if (packages.length > 0) {
-      console.warn(
-        `The following packages will fail to publish, please add access for ${user}:`
-      )
-      console.warn(packages.map(pkg => ` - ${pkg.name}`).join(`\n`))
-      process.exit(1)
-    }
-  })
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
+(async function() {
+try {
+const { packages, user } = await getUnownedPackages();
+
+if (packages.length > 0) {
+console.warn(
+`The following packages will fail to publish. Please add access for ${user}:\n` +
+packages.map(pkg => ` - ${pkg.name}`).join('\n')
+);
+process.exit(1);
+} else {
+console.log('All packages have the necessary access.');
+}
+} catch (error) {
+console.error('An error occurred while checking package ownership:', error);
+process.exit(1);
+}
+})();
